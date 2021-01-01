a7ffe298ca81a42d9a4895b829bfa316d3839a3f
claudiolammirato <claudio.lammirato@gmail.com>
2021-01-01 17:36:29 +0100

    init

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const
+var express = require('express');
+var exphbs  = require('express-handlebars');
+ 
+var app = express();
+ 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
+ 
+app.get('/', function (req, res) {
+    res.render('index');
+});
+ 
+app.listen(3000, () => {
+    console.log(`Example app listening at http://localhost:3000`)
+  })
